25499740260336c2018013b911a5cd39edb32a69
glowiak <52356948+glowiak@users.noreply.github.com>
2020-07-05 17:46:48 +0200

    Create AAI.sh
    ncn

---
+++ AAI.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Advanced Aur Installer (AAI) by glowiaK
+echo To use this tool you must install "base-devel" and "git" packages.
+echo To install it type as root:
+echo pacman -S base-devel git
+echo -n "AUR package name: "
+read pkgn
+git clone https://aur.archlinux.org/${pkgn}.git
+cd $pkgn
+makepkg -si
+echo "Done installing ${pkgn}!"
